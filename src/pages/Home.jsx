9869193a525abfcdcdafe6886eb04bc2f1415d3e
Ciprian BRAT <ciprian.brat@indsoft.ro>
2021-10-26 15:20:00 +0300

    Added Routes and Links from rrd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Home() {
+function Home(props) {
+  console.log(props);
+  
   return(
     <div>
+      <div>
       Home
+      </div>
+      <Link to="/login">Login</Link>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return(
     <div>
       Login
+      <Link to="/">Home</Link>
+
     </div>
   )
 }
