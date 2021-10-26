5d9543311570d9033a2d2d6d75388bd90c399f63
Ciprian BRAT <ciprian.brat@indsoft.ro>
2021-10-26 13:43:39 +0300

    New components, new pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
+
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Page404 from './pages/Page404'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="*" component={Page404}/>
+      </Switch>
     </div>
   );
 }
