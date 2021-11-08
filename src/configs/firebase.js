42bc72164ecf827dd98cf6f4c348deb6a526b05c
Ciprian BRAT <ciprian.brat@indsoft.ro>
2021-11-08 15:44:48 +0200

    firebase

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbTngyanzbQqaHIgsGPiN45aNm7e7kfCk",
+  authDomain: "react-eshop-01.firebaseapp.com",
+  projectId: "react-eshop-01",
+  storageBucket: "react-eshop-01.appspot.com",
+  messagingSenderId: "584259317101",
+  appId: "1:584259317101:web:c881c6f371f9e123cebe64"
+};
+
+export default firebaseConfig;
